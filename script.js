34a83d4493582d18b20e67d49cc7393bc470ce69
lexus654 <ronald.laz@tup.edu.ph>
2021-08-22 20:52:07 +0800

    my commit

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const triangle = document.querySelector(".triangle");
 const triangleID = document.getElementById("triangle");
 const container = document.getElementById("container");
 const screen2 = document.getElementById("screen-2");
+const loginScreen = document.getElementById("login-screen");
+
+const loginScreenToggle = document.getElementById("loginScreen");
+const homeScreenToggle = document.getElementById("homeScreen");
 let condition = false;
-console.log(condition);
 
 // This function will change the background of the rectangle
 const changeBackground = function (id, color) {
@@ -22,6 +25,11 @@ const transition = function (id, time = "1.3s") {
 const transformTranslateY = function (id) {
   document.getElementById(`${id}`).style.transform = "translateY(-80px)";
 };
+// this function is for opacity
+const opacity = function (id, value) {
+  document.getElementById(`${id}`).style.opacity = `${value}`;
+};
+
 // click event//////////////////////////////////////////////////////////////////////////
 triangle.addEventListener("click", function () {
   // rect div box
@@ -41,8 +49,24 @@ triangle.addEventListener("click", function () {
   if (condition === true) {
     setTimeout(() => {
       document.getElementById(`screen-2`).style.opacity = "10";
-      transition("screen-2", "2s");
+      transition("screen-2", "1s");
       console.log(condition);
     }, 1000);
   }
+});
+
+loginScreen.addEventListener("click", function () {
+  console.log("Nagana");
+  opacity("homeScreen", 0);
+  transition("homeScreen");
+  setTimeout(() => {
+    loginScreenToggle.classList.remove("toggler");
+    homeScreenToggle.classList.add("toggler");
+    console.log("part2");
+  }, 500);
+  setTimeout(() => {
+    opacity("loginScreen", 10);
+    transition("loginScreen");
+    console.log("part3");
+  }, 1000);
 });
